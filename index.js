5558c9b3a3c8a01c6d55d3e33499348225be6795
Hulkhere <vishnubhupathiamudha@gmail.com>
2021-10-06 07:38:52 +0530

    initial-commit

--- index.js
+++ index.js
@@ -15,3 +15,55 @@ for (i = 0; i < acc.length; i++) {
 
 }
 
+const start = document.querySelector("#start");
+const pause = document.querySelector("#pause");
+const update = document.querySelector("#update");
+var minDigits = document.getElementById("min");
+var secDigits = document.getElementById("sec");
+
+
+startMins = 10;
+
+var countSec = startMins*60;
+var timerVar = null;
+
+var countdownIsRunning = false;
+
+function startCounter() {
+  countdownIsRunning = true;
+  --countSec;
+
+  
+  mins = Math.floor(countSec  / 60);
+  secs = Math.floor(countSec %60);
+
+  if (secs < 10) {
+    secDigits.innerHTML = "0" + secs;
+  } else {
+    secDigits.innerHTML = secs;
+  }
+  if ( secs === 1){
+    countdownIsRunning=false;
+  }
+  console.log(`${mins} ${secs}`);
+
+  minDigits.innerHTML = "0" + mins;
+}
+
+start.addEventListener("click", () => {
+  if (countdownIsRunning == false){
+    timerVar = setInterval(startCounter, 1000);
+  }
+});
+
+pause.addEventListener("click", () => {
+  if (timerVar) clearInterval(timerVar);
+});
+
+update.addEventListener("click", () => {
+  countSec = mins;
+  secDigits.innerHTML = "00";
+  minDigits.innerHTML = mins;
+  clearInterval(timerVar);
+});
+
